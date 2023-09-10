4bb19643c2a8f511ae1026e9790ffa2229bb12ef
Saccilotto <andre.santos01@edu.pucrs.br>
2023-09-10 01:32:23 -0300

    Added sample project files dir appstart

---
+++ appstart/src/test/java/com/quickstart/appstart/AppstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickstart.appstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
